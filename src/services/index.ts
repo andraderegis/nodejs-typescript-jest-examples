305791d43eeb37ede2fdd0155b342947722f079f
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-12-31 16:06:11 -0300

    refactor: rename file name

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export * from './create-account-service-with-di';
 export * from './create-account-service-without-di';
 export * from './create-account-service-functional-with-di';
-export * from './create-account-service-function-without-di';
+export * from './create-account-service-functional-without-di';
